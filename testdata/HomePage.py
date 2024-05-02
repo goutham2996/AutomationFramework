3b1d5addfa949d3da29244306196e6e78067d9ba
goutham2996 <gautamspikee@gmail.com>
2024-05-02 10:12:25 +0530

    datadriven

---
+++ testdata/HomePage.py
@@ -0,0 +1,4 @@
+class HomePageTestData:
+
+    test_data_test_Form = [{"uname":"Goutham", "uemail":"gautamspikee@gmail.com", "upassword":"abc123", "ugender":"Male", "udob":"29081996"},
+                            {"uname":"Gouthami", "uemail":"gautamspikee06@gmail.com", "upassword":"abc321", "ugender":"Female", "udob":"29091996"}]
